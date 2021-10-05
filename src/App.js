2522aaedf47ef75eaed021ede13163fd13e089bd
megan <megan.schertz@gmail.com>
2021-10-05 11:45:54 -0500

    initial state for books set

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, {Component } from 'react'
 
 
 class App extends Component {
+  state = {
+    books: []
+  }
   componentDidMount() {
     fetch('http://localhost:3000/books')
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(data => this.setState({books: data}))
+    
   }
  
   render() {
+    console.log(this.state)
     return (
       <div>
-
+        {this.state.books.map(book => 
+        <div>
+          <h2>{book.title} </h2>
+          <h3> {book.author.name} </h3>
+          <h3> {book.genre.name} </h3>
+          <p> {book.description} </p>
+        </div>
+        )}
       </div>
     )
   }
